482a285284dfc9685cf7da96322d44d9df162e4e
Fellipe Thufik <fellipe_thufik@hotmail.com>
2017-01-10 20:55:20 -0200

    Alterações necessárias tem que ser feitas, está com bug

--- bot.py
+++ bot.py
@@ -1,17 +1,31 @@
 import requests,json,time
 
-while(1):
-	time.sleep(20)
 
-	r = requests.get('https://pagarme.zendesk.com/api/v2/views/39073217/tickets.json',auth=('fellipe.thufik@pagar.me','thufik19'))
+r = requests.get('https://pagarme.zendesk.com/api/v2/views/39073217/tickets.json',auth=('fellipe.thufik@pagar.me','thufik19'))
 
-	jayson = json.loads(r.text)
-	count = jayson['count']
-
-	if count > 0:
-		
-		message = "<!here> Olha o zendesk, tem {0} tickets novos".format(count)
-		requests.post("https://slack.com/api/chat.postMessage?token=xoxp-2465752868-87168214994-124135080481-9c2f8f0ce5fa52664403fe521dcb4e97&channel=testbotzendesk&text={0}".format(message))
+jayson = json.loads(r.text)
+count = jayson['count']
 
 
+if count > 0:
+
+	tickets = jayson['tickets']
+	file = open("newfile.txt").read()
+	newTickets = 0
+
+	for ticket in tickets:
+		if str(ticket['id']) in file:
+			print 'x'
+		else:
+			newTickets = newTickets + 1
+			open("newfile.txt","w").write(str(ticket['id']))
+
+
+	message = "<!here> Olha o zendesk, tem {0} tickets novos".format(newTickets)
+	requests.post("https://slack.com/api/chat.postMessage?token=xoxp-2465752868-87168214994-124135080481-9c2f8f0ce5fa52664403fe521dcb4e97&channel=testbotzendesk&text={0}".format(message))
+
+else:
+	message = "<!here> Nao tem nenhuma ticket novo"
+	requests.post("https://slack.com/api/chat.postMessage?token=xoxp-2465752868-87168214994-124135080481-9c2f8f0ce5fa52664403fe521dcb4e97&channel=testbotzendesk&text={0}".format(message))
+	open("newfile.txt","w").close()
 
